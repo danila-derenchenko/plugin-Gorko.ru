8b035092f3b57a496bd0c4b8e9c40d40c01d9e30
danila-derenchenko <danila.derenchenko@gmail.com>
2024-01-11 12:38:09 +0400

    update

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const openForm = document.querySelector("#openFormReminder")
 const formReminder = document.querySelector("#formReminder")
+const inputURL = document.getElementById("formReminderURL")
+const formReminderWrapper = document.getElementById("formReminderWrapper")
 
 formReminder.addEventListener("submit", () => {alert("submit")})
 
 openForm.addEventListener("click", () => {showForm(document.URL)})
 
 const showForm = (URLink) => {
-    
+    inputURL.value = document.URL
+    formReminderWrapper.style = "display:flex"
 }
