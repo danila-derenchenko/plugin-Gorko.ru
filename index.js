a6828d6306bba8c03691ac9b0223e7fd70a7719e
danila-derenchenko <danila.derenchenko@gmail.com>
2024-01-16 15:22:04 +0400

    add auto datatime

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
+/* Константы */
+
 const openForm = document.querySelector("#openFormReminder")
 const formReminder = document.querySelector("#formReminder")
 const inputURL = document.getElementById("formReminderURL")
+const inputText = document.getElementById("formReminderText")
+const inputDate = document.querySelector("#formReminderDate")
 const formReminderWrapper = document.getElementById("formReminderWrapper")
 
-formReminder.addEventListener("submit", () => {alert("submit")})
+formReminder.addEventListener("submit", (event) => {
+    event.preventDefault()
+    formReminderWrapper.style = "display: none"
+    sendTask()
+})
 
 openForm.addEventListener("click", () => {showForm(document.URL)})
 
 const showForm = (URLink) => {
-    inputURL.value = document.URL
-    formReminderWrapper.style = "display:flex"
+    formReminderWrapper.style = "display: flex"
+    inputURL.value = URLink
+    const nowDatetime = new Date
+    inputDate.value = `${checkDataTimeFormat(nowDatetime.getFullYear())}-${checkDataTimeFormat(nowDatetime.getMonth() + 1)}-${checkDataTimeFormat(nowDatetime.getDate())}T${checkDataTimeFormat(nowDatetime.getHours() + 1)}:${checkDataTimeFormat(nowDatetime.getMinutes())}`
+    console.log(inputDate.value)
+}
+
+const sendTask = () => {
+    const data = JSON.stringify({
+        dateTime: inputDate.value,
+        URLaddres: inputURL.value,
+        text: inputText.value
+    })
+    console.log(data)
+    inputText.value = ""
+    inputDate.value = ""
+}
+
+const checkDataTimeFormat = (datetime) => {
+    if(datetime < 10) {
+        return `0${datetime}`
+    } else {
+        return `${datetime}`
+    }
 }
