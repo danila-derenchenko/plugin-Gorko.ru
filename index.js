67a78ca36682daa22a11fba94e3bc7d650e62364
danila-derenchenko <danila.derenchenko@gmail.com>
2024-01-22 23:43:20 +0400

    add initualization

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-/* Запросы к серверу */
-
-fetch('https://raw.githubusercontent.com/danila-derenchenko/forApi/main/data.json').then((res) => res.json()).then(rerult => console.log(rerult)).catch(() => console.error("Ошибка получения данных"))
-
-/* Константы */
+/* Константы и переменные */
 
 const openForm = document.querySelector("#openFormReminder")
 const formReminder = document.getElementById("formReminder")
@@ -11,16 +7,6 @@ const inputText = document.getElementById("formReminderText")
 const inputDate = document.querySelector("#formReminderDate")
 const formReminderWrapper = document.getElementById("formReminderWrapper")
 const showReminder = document.getElementById("showReminder")
-
-/* Логика */
-
-showReminder.style = "display: none"
-if(localStorage.getItem("countPages") == null || JSON.parse(localStorage.getItem("countPages")) <= 0) {
-    localStorage.setItem("countPages", JSON.stringify(1))
-} else {
-    const countPages = JSON.parse(localStorage.getItem("countPages"))
-    localStorage.setItem("countPages", JSON.stringify(countPages + 1))
-}
 
 /* Слушатели событий */
 
@@ -33,12 +19,27 @@ window.addEventListener("beforeunload", () => {
 formReminder.addEventListener("submit", (event) => {
     event.preventDefault()
     formReminderWrapper.style = "display: none"
-    sendTask()
+    const data = {
+        dateTime: inputDate.value,
+        URLaddres: inputURL.value,
+        text: inputText.value
+    }
+    console.log(data)
+    inputText.value = ""
+    inputDate.value = ""
+    addTask(data)
 })
 
 openForm.addEventListener("click", () => {showForm(document.URL)})
 
 /* Функции */
+
+const addTimerTask = (task) => {
+    const nowDateTime = new Date
+    const nowTime = new Date(`${checkDataTimeFormat(nowDateTime.getFullYear())}-${checkDataTimeFormat(nowDateTime.getMonth() + 1)}-${checkDataTimeFormat(nowDateTime.getDate())}T${checkDataTimeFormat(nowDateTime.getHours())}:${checkDataTimeFormat(nowDateTime.getMinutes())}`)
+    const taskTime = new Date(task.dateTime)
+    setTimeout(() => {console.log("timer")}, taskTime - nowTime)
+}
 
 const showForm = (URLink) => {
     inputURL.value = URLink
@@ -55,20 +56,39 @@ const checkDataTimeFormat = (datetime) => {
     }
 }
 
-const sendTask = () => {
-    const data = {
-        dateTime: inputDate.value,
-        URLaddres: inputURL.value,
-        text: inputText.value
-    }
-    console.log(data)
-    inputText.value = ""
-    inputDate.value = ""
+const addTask = (task) => {
+    console.log(task)
+    addTimerTask(task)
     if(localStorage.getItem("tasksList") != null) {
         let localstorageData = JSON.parse(localStorage.getItem("tasksList"))
-        localstorageData.push(data)
+        localstorageData.push(task)
         localStorage.setItem("tasksList", JSON.stringify(localstorageData))
     } else {
-        localStorage.setItem("tasksList", JSON.stringify([data]))
+        localStorage.setItem("tasksList", JSON.stringify([task]))
     }
+}
+
+const initualizationTaskList = (taskList) => {
+    console.log(taskList)
+    for(let i = 0; i < taskList.length; i++) {
+        addTask(taskList[i])
+    }
+}
+
+/* Логика */
+
+/* Запросы к серверу */
+
+/* Получение уже созданных задач, и установка таймеров */
+fetch('https://raw.githubusercontent.com/danila-derenchenko/forApi/main/data.json').then((res) => res.json()).then(result => {
+    initualizationTaskList(result)
+}).catch(() => console.error("Ошибка получения данных"))
+
+
+showReminder.style = "display: none"
+if(localStorage.getItem("countPages") == null || JSON.parse(localStorage.getItem("countPages")) <= 0) {
+    localStorage.setItem("countPages", JSON.stringify(1))
+} else {
+    const countPages = JSON.parse(localStorage.getItem("countPages"))
+    localStorage.setItem("countPages", JSON.stringify(countPages + 1))
 }
